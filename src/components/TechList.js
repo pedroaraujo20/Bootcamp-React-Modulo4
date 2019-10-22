90e5324e110b8b556638fcf83644577c3a8d7311
Pedro Araujo <pedro-foa@hotmail.com>
2019-10-21 23:52:53 -0300

    Finished module 4

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -3,14 +3,34 @@ import React, { Component } from 'react';
 import TechItem from './TechItem';
 
 class TechList extends Component {
+  static defaultProps = {
+
+  };
+
+  static propTypes = {
+
+  };
+
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
   
   handleInputChange = e => {
     this.setState({ newTech: e.target.value});
@@ -34,7 +54,12 @@ class TechList extends Component {
       <>
         <form onSubmit={this.handleSubmit}>
           <ul>
-            {this.state.techs.map(tech => <TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)}/>)}
+            {this.state.techs.map(tech => 
+            <TechItem 
+              key={tech} 
+              tech={tech} 
+              onDelete={() => this.handleDelete(tech)}
+            />)}
           </ul>
           <input 
             type="text" 
